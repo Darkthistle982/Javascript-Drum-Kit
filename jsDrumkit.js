e0ca38a9f0d6bb93a16c192d0bc85dc73807dd94
Brian Allen <brianallen2401@gmail.com>
2020-10-30 19:10:38 -0600

    tinkering with the transitionend. This function won't fire. Needs more testing.

--- jsDrumkit.js
+++ jsDrumkit.js
@@ -1,16 +1,18 @@
-window.addEventListener("keydown", function (e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+function removeTransition(event) {
+  if (event.propertyName !== "transform") return;
+  event.target.classList.remove("playing");
+  console.log(event)
+}
+
+function playSound(event) {
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
   key.classList.add("playing");
-});
-
-function removeTransition(e) {
-  if (e.propertyName !== "transform") return;
-  this.classList.remove('playing');
 }
 
 const keys = document.querySelectorAll(".key");
-keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
